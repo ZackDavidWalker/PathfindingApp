cc1848893c248c02948cf1b90e8c11ddcf792c38
David Zack Walker <davidzzwalker@gmail.com>
2019-05-25 11:53:57 +0200

    initial commit

---
+++ src/main/java/PathfindingApp/App.java
@@ -0,0 +1,12 @@
+package PathfindingApp;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+
+    }
+}
